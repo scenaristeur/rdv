bb3956fc6755da57292b37259e07478d917d918e
scenaristeur <scenaristeur@gmail.com>
2023-07-23 05:16:00 +0200

    earthquakes legend hs

--- src/components/LayerPanel.vue
+++ src/components/LayerPanel.vue
@@ -1,25 +1,35 @@
 <template>
     <div>
-        
-        <q-tabs
-            v-model="tab"
-            inline-label
-            class="bg-green text-white"
-        >
+        <q-tabs v-model="tab" inline-label class="bg-green text-white">
             <q-tab name="layers" icon="layers" label="Layers" />
-            
-        </q-tabs>           
-        <q-list v-if="map != null" separator bordered>
-            <q-item clickable v-ripple :key="layer.index" v-for="(layer) in map.getLayers().getArray().slice().reverse()" style="height: 10px;">
-                <q-item-section avatar>
-                    <q-checkbox color="primary" v-model = "layer.getProperties().visible" @click = "toggleLayer(layer)"/>
-                </q-item-section>
 
-                <q-item-section class = "label_align">            
-                    {{layer.get('name')}}            
-                </q-item-section>            
-            </q-item>
-        </q-list>
+        </q-tabs>
+
+        <span v-if="map != null">
+            <q-list separator bordered clickable :key="index"
+                v-for="(layer, index) in map.getLayers().getArray().slice().reverse()">
+                <q-item style="height: 10px;">
+                    <q-item-section avatar>
+                        <!-- <q-checkbox color="primary" v-model = "layer.getProperties().visible" @click = "toggleLayer(layer)"/> -->
+                        <span class="material-symbols-outlined cursor-pointer"
+                            style="color: #54b582; font-size: 40px; font-variation-settings: 'FILL' 1;"
+                            @click="toggleLayer(layer, $event)" v-if="!layer.get('isBaseMap')">
+                            check_circle
+                        </span>
+                    </q-item-section>
+
+                    <q-item-section class="label_align">
+                        {{ layer.get('name') }}
+                    </q-item-section>
+                    <q-item-section avatar>
+                        <q-icon name="keyboard_arrow_down" class="expansion_align cursor-pointer" size="25px"
+                            @click="toggleSwitch(layer, $event)" v-if="!layer.get('isBaseMap')" />
+                    </q-item-section>
+                </q-item>
+                <q-separator />
+                <InLegend :layer="layer" />
+            </q-list>
+        </span>
     </div>
 </template>
 
@@ -29,47 +39,24 @@
 import { Vector as VectorSource } from 'ol/source';
 import { VectorImage } from 'ol/layer';
 import GeoJSON from 'ol/format/GeoJSON';
+import OpenLayersParser from "geostyler-openlayers-parser";
+import layers from "../assets/layers/layers"
+import InLegend from "./InLegend";
 
-// Import geojson layers
-import us_states from '../assets/geojsons/us_states.json';
-import us_counties from '../assets/geojsons/us_counties.json';
-import us_outline from '../assets/geojsons/us_outline.json';
-import earthquake from '../assets/geojsons/earthquake.json';
 
-console.log(us_states)
 
-let layers = [
-    {
-        "layer": us_states,
-        "name": 'US States',
-        "visible": true
-        
-    },
-    {
-        "layer": us_counties,
-        "name": 'US Couties',
-        "visible": true
-    },
-    {   
-        "layer": us_outline,
-        "name": 'US Outline',
-        "visible": true
-    },
-    {
-        "layer": earthquake,
-        "name": 'Earthquakes',
-        "visible": true
-    }
-]
-
+const parser = new OpenLayersParser();
 
 export default {
-    name: 'LayersPanel',   
+    name: 'LayersPanel',
     data() {
         return {
             splitterModel: 1,
             tab: 'mails'
         }
+    },
+    components: {
+        InLegend
     },
     watch: {
         'map': {
@@ -80,34 +67,62 @@ export default {
         }
     },
     computed: {
-        map() {            
+        map() {
             return this.$store.state.map
         }
     },
-    methods: {        
-        addLayers() {    
-            
-            layers.forEach(lyr =>{
-                console.log(lyr.layer);
-                this.map.addLayer(
-                    new VectorImage({
-                        source: new VectorSource({
-                            features: new GeoJSON({'featureProjection': 'EPSG:3857'}).readFeatures(lyr.layer)
-                        }),
-                        name: lyr.name
-                    })
-                )
+    methods: {
+        addLayers() {
+            layers.forEach(lyr => {
+                let current_layer = new VectorImage({
+                    source: new VectorSource({
+                        features: new GeoJSON({ 'featureProjection': 'EPSG:3857' }).readFeatures(lyr.layer)
+                    }),
+                    name: lyr.name,
+                    minZoom: lyr.minZoom,
+                    isExpanded: false
+                });
+                current_layer.geostyle = lyr.style;
+                current_layer.geostyleTwin = JSON.parse(JSON.stringify(lyr.style));
+                if (lyr.style) {
+                    parser
+                        .writeStyle(lyr.style)
+                        .then(({ output: olStyle }) => current_layer.setStyle(olStyle))
+                        .catch(error => console.log(error));
+                }
+                this.map.addLayer(current_layer);
             });
         },
-        toggleLayer(layer) {
-            layer.setVisible(!layer.getVisible());
+        toggleLayer(layer, e) {
+            let flag = layer.getVisible();
+            layer.setVisible(!flag);
+            if (flag == false) {
+                e.target.style.fontVariationSettings = '"FILL" 1';
+            }
+            else {
+                e.target.style.fontVariationSettings = '"FILL" 0';
+            }
+        },
+        toggleSwitch(layer, e) {
+            let flag = layer.get('isExpanded');
+            layer.set('isExpanded', !flag);
+            if (flag == false) {
+                e.target.innerHTML = "keyboard_arrow_up";
+            }
+            else {
+                e.target.innerHTML = "keyboard_arrow_down";
+            }
         }
     }
-    
+
 }
 </script>
 <style scoped>
-    .label_align {
-        text-align: left;
-    }
+.label_align {
+    text-align: left;
+}
+
+.expansion_align {
+    text-align: right;
+}
 </style>
