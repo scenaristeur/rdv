9ceea7fce656ac9e29cb594626b2c97e1443dfd9
scenaristeur <scenaristeur@users.noreply.github.com>
2023-11-18 14:49:26 +0000

    deploy: fb55abc55be694168c56c4d1a836ebc959398713

--- js/about.1b422418.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkrdv"]=self["webpackChunkrdv"]||[]).push([[443],{1576:function(n,r,u){u.r(r),u.d(r,{default:function(){return f}});var t=u(3396);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page ",-1),s=[e];function c(n,r){return(0,t.wg)(),(0,t.iD)("div",a,s)}var i=u(89);const o={},d=(0,i.Z)(o,[["render",c]]);var f=d}}]);
-//# sourceMappingURL=about.1b422418.js.map
